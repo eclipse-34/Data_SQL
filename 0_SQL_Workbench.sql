b335aadadd72fc9dfb1f59bf9eb3ae8c2743d382
eclipse-34 <109349939+eclipse-34@users.noreply.github.com>
2023-09-15 18:44:06 +0900

    Update 0_SQL_Workbench.sql

--- 0_SQL_Workbench.sql
+++ 0_SQL_Workbench.sql
@@ -1,16 +1,13 @@
-/*
 
-Ctrl + T : 새로운 쿼리 창 열기
-Ctrl + Enter : 쿼리 실행
-Ctrl + s : 쿼리 저장
-(Mac OS에서는 Ctrl 대신 Command)
+# Ctrl + T : 새로운 쿼리 창 열기
+# Ctrl + Enter : 쿼리 실행
+# Ctrl + s : 쿼리 저장
+# (Mac OS에서는 Ctrl 대신 Command)
 
-SQL 쿼리는 기본적으로 한 번에 한 문장이 실행되므로
-실행하려는 SQL문을 선책하거나 해당 줄에 커서를 놓고 실행 버튼을 누르거나  Ctrl + Enter
+# SQL 쿼리는 기본적으로 한 번에 한 문장이 실행되므로
+# 실행하려는 SQL문을 선책하거나 해당 줄에 커서를 놓고 실행 버튼을 누르거나  Ctrl + Enter
 
-SQL에서는 대소문자 구분 없음
-
-*/
+# SQL에서는 대소문자 구분 없음
 
 # 데이터베이스 생성
 CREATE DATABASE TEST;
